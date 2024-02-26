145d66de4bbd81d30fc3d861486fbd72cc504fe7
HungHT1890 <hungsaki2003@gmail.com>
2024-02-26 13:36:28 +0700

    bypass cookie when crawl data

---
+++ Crawl/define.py
@@ -0,0 +1,23 @@
+api = 'https://www.facebook.com/api/graphql/'
+header = {
+        'Accept':'*/*',
+        'Accept-Language':'en-US,en;q=0.9',
+        'Content-Length':'1426',
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Dpr':'1',
+        'Sec-Ch-Prefers-Color-Scheme':'dark',
+        'Sec-Ch-Ua':'"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+        'Sec-Ch-Ua-Full-Version-List':'"Not A(Brand";v="99.0.0.0", "Google Chrome";v="121.0.6167.189", "Chromium";v="121.0.6167.189"',
+        'Sec-Ch-Ua-Mobile':'?0',
+        'Sec-Ch-Ua-Model':'""',
+        'Sec-Ch-Ua-Platform':'"Windows"',
+        'Sec-Ch-Ua-Platform-Version':'"10.0.0"',
+        'Sec-Fetch-Dest':'empty',
+        'Sec-Fetch-Mode':'cors',
+        'Sec-Fetch-Site':'same-origin',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+        'Viewport-Width':'706',
+        'X-Asbd-Id':'129477',
+        'X-Fb-Friendly-Name':'ProfileCometAppCollectionListRendererPaginationQuery',
+        'X-Fb-Lsd':'iLcfw4DDEXHv6uqlzWs17O',
+        }
---
+++ Crawl/test.py
@@ -0,0 +1,5 @@
+from base64 import b64encode
+
+uid = 'app_collection:61550915105047:2356318349:2'
+
+print()
